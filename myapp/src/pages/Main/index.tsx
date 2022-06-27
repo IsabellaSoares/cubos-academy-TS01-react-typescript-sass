2143b1cde193f5995964a22e7baa962793f632fa
IsabellaSoares <isabella.soares.28@gmail.com>
2022-06-27 15:02:08 -0300

    add new architecture

---
+++ myapp/src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Hello world!
+      </header>
+    </div>
+  );
+}
+
+export default Main;
