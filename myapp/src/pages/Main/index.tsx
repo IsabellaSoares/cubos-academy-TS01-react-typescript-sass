d572d8fa2f743c6a62a011415677d0abb621e5c3
IsabellaSoares <isabella.soares.28@gmail.com>
2022-06-28 21:39:59 -0300

    add subtitle and layout tokens

--- myapp/src/pages/Main/index.tsx
+++ myapp/src/pages/Main/index.tsx
@@ -1,3 +1,10 @@
-const Main = () => <h2>Main</h2>;
+import styles from "./styles.module.scss";
+
+const Main = () => (
+  <main className={styles.container}>
+    <h2 className={styles.subtitle}>Seus produtos</h2>
+    <button className="btn-blue">Adicionar produto</button>
+  </main>
+);
 
 export default Main;
