f9c14d19dbbb2336969444ecf0c6a101cf093e61
IsabellaSoares <isabella.soares.28@gmail.com>
2022-07-01 15:15:55 -0300

    fix header layout

--- myapp/src/layouts/HeaderLayout/index.tsx
+++ myapp/src/layouts/HeaderLayout/index.tsx
@@ -1,16 +1,28 @@
 import { Outlet } from "react-router-dom";
+import ConfirmModal from "../../components/ConfirmModal";
 import { Header } from "../../components/Header";
-
+import useGlobalContext from "../../hooks/useGlobalContext";
 import styles from "./styles.module.scss";
 
-const HeaderLayout = () => (
-  <>
-    <Header />
-    <div className={styles.contentPage}>
-      <h1 className={styles.title}>Nome da Loja</h1>
-      <Outlet /> 
+const HeaderLayout = () => {
+  const { handleConfirmModal, openModal, handleCloseModal } =
+    useGlobalContext();
+
+  return (
+    <div className={styles.container}>
+      <Header />
+      <main className={styles.contentPage}>
+        <h1>Loja do Dani</h1>
+        <Outlet />
+      </main>
+
+      <ConfirmModal
+        open={openModal}
+        handleClose={handleCloseModal}
+        handleConfirm={handleConfirmModal}
+      />
     </div>
-  </>
-);
+  );
+}
 
 export default HeaderLayout;
